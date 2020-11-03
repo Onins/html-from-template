a136d9196fc65b8bc4fd2dacd21920389819168f
Nino Onril Gamboa <nino_gamboa@ligph.com>
2020-11-03 13:36:35 +0800

    Partial progress for WHM automation

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,80 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <form enctype="multipart/form-data">
+        <input type="file" @change="onFileChange">
+        <a @click="downloadHtml()">Button</a>
+    </form>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import JSZip from 'jszip'
+import { saveAs } from 'file-saver'
+import WorldHeritage from "./class/whm.js"
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  name: "App",
+  data() {
+    return {
+      fileinput: '',
+      jsonoutput: []
+    }
+  },
+  methods: {
+    onFileChange(e) {
+      this.jsonoutput = [];
+      this.fileinput = "";
+      var file = e.target.files;
+      this.createInput(file[0]);
+    },
+
+    createInput(file) {
+      let vm = this;
+      let promise = new Promise((resolve) => {
+        let reader = new FileReader();
+        reader.onload = () => {
+          resolve((vm.fileinput = reader.result));
+        };
+        reader.readAsText(file);
+      });
+
+      promise.then(
+        () => {
+          let lines=vm.fileinput.split("\n");
+          let headers=lines[0].split(",");
+
+          for(let i=1;i<lines.length - 1;i++){
+            let obj = {};
+            let currentline=lines[i].split(",");
+
+            for(let j=0;j<headers.length;j++){
+              obj[headers[j]] = currentline[j];
+            }
+            this.jsonoutput.push(JSON.parse(JSON.stringify(obj).replace(/\\n|\\r/g, '')));
+          }
+        },
+        error => {
+          console.log(error);
+        }
+      );
+    },
+
+    downloadHtml() {  
+      let zip = new JSZip();
+      let vm = this;
+      for(let i = 0; i < vm.jsonoutput.length; i++) {
+        zip.file(`${vm.jsonoutput[i].destination}.html`, this.updateTemplate(vm.jsonoutput[i]));
+      }
+      zip.generateAsync({type:"blob"})
+      .then(function(content) {          
+        saveAs(content, "world-heritage.zip");
+      });       
+    },
+
+    updateTemplate(data) {
+      let country = new WorldHeritage(data.month, data.airport, data.airplane, data.destination);
+      return country.getTemplate();
+    }
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/class/whm.js
@@ -0,0 +1,26 @@
+export default class whm {
+  month = "";
+  airport = "";
+  airplane = "";
+  destination = "";
+
+  constructor(month, airport, airplane, destination) 
+  {
+    this.month = month;
+    this.airport = airport;
+    this.airplane = airplane;
+    this.destination = destination;
+  }
+  getTemplate()
+  {
+    return `
+      <!DOCTYPE html>
+      <html lang="ja">
+        <p>Month = ${this.month}</p>
+        <p>Airport = ${this.airport}</p>
+        <p>Plane = ${this.airplane}</p
+        <p>Destination = ${this.destination}</p>
+      </html>
+    `;
+  }
+}
